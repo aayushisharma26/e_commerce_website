bc0baec4b1bde0c7320f76574ad8bb9a0b850926
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-11-29 11:57:30 +0530

    remove some data

--- app/navbar1.js
+++ app/navbar1.js
@@ -2,9 +2,8 @@ import React from "react";
 
 function Navbar1() {
   return (
-    <nav className="navbar bg-body-tertiary">
-      <div className="container-fluid" style={{ backgroundColor: "#4B429E", height: "54px", margin: "13px" }}>
-        {/* Use the <img> tag to display the image */}
+    <nav className="navbar bg-primary">
+      <div className="container-fluid" style={{height: "54px"}}>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTws27MDkRYwBLQf2llMfC3eSvjQ3WF8C6fNw&usqp=CAU" alt="Logo" style={{ width: "50px", height: "50px" }} />
 
         <form className="d-flex" role="search" style={{ marginLeft: "365px" }}>
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+// "use client"
 // import React from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import  Electronics_data from './product_data';
@@ -27,9 +28,9 @@
 
 
 
-
-
 "use client"
+
+
 
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -57,8 +58,8 @@ function Page() {
       {categoryData.slice(0, 4).map((item) => (
         <div className="col-md-3" key={item.id}>
           <div style={{ border: '1px solid red', padding: '10px', marginBottom: '10px' }}>
-            <h1>{item.title}</h1>
-            <img src={item.image} alt={item.title} className="img-fluid" style={{ border: '1px solid red', width: "100%" }} />
+            {/* <p>{item.title}</p> */}
+            <img src={item.image} alt={item.title} className="img-fluid" style={{ border: '1px solid red', width: "10%" }} />
           </div>
         </div>
       ))}
@@ -83,53 +84,12 @@ export default Page;
 
 
 
-// "use client"
 
-// import React, { useState, useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
-// function Page() {
-//   const [data, setData] = useState([]);
 
-//   const fetchData = () => {
-//     fetch("https://fakestoreapi.com/products")
-//       .then((res) => res.json())
-//       .then((res) => {
-//         setData(res);
-//       })
-//       .catch((error) => {
-//         console.error("Fetch error:", error);
-//       });
-//   };
 
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
 
-//   const renderCategoryRow = (categoryData) => (
-//     <div className="row">
-//       {categoryData.slice(0, 4).map((item) => (
-//         <div className="col-md-3" key={item.id}>
-//           <div style={{ border: '1px solid red', padding: '10px', marginBottom: '10px' }}>
-//             <h1>{item.title}</h1>
-//             <img src={item.image} alt={item.title} className="img-fluid" style={{ border: '1px solid red', width: "100%" }} />
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
 
-//   const electronicData = data.filter((item) => item.category === "electronics");
-//   const jewelryData = data.filter((item) => item.category === "jewelery");
-//   const womensData = data.filter((item) => item.category === "women's clothing");
 
-//   return (
-//     <>
-//       {renderCategoryRow(electronicData)}
-//       {renderCategoryRow(jewelryData)}
-//       {renderCategoryRow(womensData)}
-//     </>
-//   );
-// }
 
-// export default Page;
+
