852e23656377756282b869b25dba432ad5425ebd
Archana Singh <archana21@navgurukul.org>
2023-11-29 11:47:04 +0530

    changes in page

--- app/navbar1.js
+++ app/navbar1.js
@@ -1,10 +1,26 @@
+// import React from "react";
+
+// function Navbar1(){
+//     return(
+//         <nav class="navbar bg-body-tertiary">
+//   <div class="container-fluid">
+//     <form class="d-flex" role="search">
+//       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+//       <button class="btn btn-outline-success" type="submit">Search</button>
+//     </form>
+//   </div>
+// </nav>
+//     )
+// }
+// export default Navbar1;
+
 import React from "react";
+
 
 function Navbar1() {
   return (
-    <nav className="navbar bg-body-tertiary">
-      <div className="container-fluid" style={{ backgroundColor: "#4B429E", height: "54px", margin: "13px" }}>
-        {/* Use the <img> tag to display the image */}
+    <nav class="navbar bg-primary" data-bs-theme="white">
+      <div className="container-fluid" style={{ height: "54px" }}>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTws27MDkRYwBLQf2llMfC3eSvjQ3WF8C6fNw&usqp=CAU" alt="Logo" style={{ width: "50px", height: "50px" }} />
 
         <form className="d-flex" role="search" style={{ marginLeft: "365px" }}>
@@ -15,5 +31,8 @@ function Navbar1() {
     </nav>
   );
 }
-
 export default Navbar1;
+
+
+
+
