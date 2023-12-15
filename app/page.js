0e68c772e38b9afc3d996f78a5f5df830bcaadfb
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-12-15 17:06:08 +0530

    Change Some Data

--- app/page.js
+++ app/page.js
@@ -1,29 +1,9 @@
 "use client"
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+import Home from "./home"
 function Page() {
-  const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const renderCategoryRows = () => {
-    const categories = {
-      electronics: data.filter((item) => item.category === "electronics").slice(0, 4),
-      jewelry: data.filter((item) => item.category === "jewelery").slice(0, 4),
-      womensClothing: data.filter((item) => item.category === "women's clothing").slice(0, 4),
-    };
+  
     return (
       <>
        
@@ -35,6 +15,5 @@ function Page() {
       </>
     );
   };
-  return <>{renderCategoryRows()}</>;
-}
+
 export default Page;
