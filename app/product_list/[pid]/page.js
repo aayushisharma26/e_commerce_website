79b2416f9240a5d70e041837473d6b523c8d9066
Archana Singh <archana21@navgurukul.org>
2023-12-22 19:22:07 +0530

    fatch data using id

---
+++ app/product_list/[pid]/page.js
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Page({ params }) {
+  const [productDetails, setProductDetails] = useState(null);
+
+  const fetchProductDetails = () => {
+    fetch(`https://fakestoreapi.com/products/${params.pid}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setProductDetails(res);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchProductDetails();
+  }, [params.pid]);
+
+  if (!productDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+    <h1>Product_list</h1>
+    <img
+        src={productDetails.image}
+        style={{ width: '350px', height: '300px', borderRadius: '8px' }}
+    />
+    <p>Product ID: {productDetails.id}</p>
+    <p>Title: {productDetails.title}</p>
+      
+    </div>
+  );
+}
+
+export default Page;
+
+
+
+    
+  
+
--- app/product_list/page.js
+++ app/product_list/page.js
@@ -1,102 +1,53 @@
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 function Product_list() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-        
+      {data.map((item) => (
+        <div key={item.id} className="col-md-3 mb-4">
+          <div style={{ borderRadius: '8px', border: '1px solid black' }}>
+            <Link to={`/product_list/${item.id}`} className="product-link">
+              <img
+                src={item.image}
+                alt={item.title}
+                style={{ width: '350px', height: '300px', borderRadius: '8px' }}
+              />
+              <div className="p-3">
+                <h6>{item.title}</h6>
+                <p>Price: ${item.price}</p>
+                <p>ID: {item.id}</p>
+              </div>
+            </Link>
           </div>
         </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 3 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 4 */}
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px', }}>
-            {/* Your content for column 1 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 2 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 3 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 4 */}
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 1 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 2 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 3 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 4 */}
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 1 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 2 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 3 */}
-          </div>
-        </div>
-        <div className="col-md-3 mb-4">
-          <div className="border border-secondary p-3" style={{ borderRadius: '5px' ,height: '400px' }}>
-            {/* Your content for column 4 */}
-          </div>
-        </div>
-      </div>
-    </div>
-  
-  </>
+      ))}
+    </>
   );
 }
 
 export default Product_list;
+
+
+
+
+
