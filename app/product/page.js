0cbc44d864c8eec26f38eaba479654e037e57b52
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-12-29 19:34:20 +0530

    Product_Detail Complete

--- app/product/page.js
+++ app/product/page.js
@@ -2,40 +2,12 @@
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ProductList = ({ category }) => {
-  const [categoryData, setCategoryData] = useState([]);
+import React from 'react'
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://fakestoreapi.com/products?category=${category}`);
-        const data = await response.json();
-        setCategoryData(data);
-      } catch (error) {
-        console.error('Fetch error:', error);
-      }
-    };
-
-    if (category) {
-      fetchData();
-    }
-  }, [category]);
-
+function Product() {
   return (
-    <div>
-      <h1>{category} Products</h1>
-      {categoryData.map((item) => (
-        <div key={item.id}>
-          <img src={item.image} alt={item.title} style={{ width: '100px', height: '100px' }} />
-          <p>{item.title}</p>
-          <p>Price: ${item.price}</p>
-          <p>ID: {item.id}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    <div> Product</div>
+  )
+}
 
-export default ProductList;
-
-
+export default Product
