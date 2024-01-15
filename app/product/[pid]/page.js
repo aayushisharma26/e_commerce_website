e609da12ceb8f9e71ec538cdcb0cda5944f312d2
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-01-15 20:25:38 +0530

    change in details page

--- app/product/[pid]/page.js
+++ app/product/[pid]/page.js
@@ -1,6 +1,64 @@
+// "use client";
+// import React, { useState, useEffect } from 'react';
+// import Cookies from 'js-cookie';
+
+// function Page({ params }) {
+  
+//   const [productDetails, setProductDetails] = useState(null);
+
+//   const fetchProductDetails = () => {
+//     fetch(`https://fakestoreapi.com/products/${params.pid}`)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         setProductDetails(res);
+//       })
+//       .catch((error) => {
+//         console.error("Fetch error:", error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     fetchProductDetails();
+//   }, [params.pid]);
+
+//   if (!productDetails) {
+//     return <div>Loading...</div>;
+//   }
+
+
+
+//   const addCart=()=>{
+//     Cookies.set("poooja","binjawa")
+
+//   }
+//   return (
+//     <div style={{ border: "solid 2px red" }}>
+//       <h1>Product Details-</h1>
+//       <img
+//         src={productDetails.image}
+//         style={{ width: '350px', height: '300px', borderRadius: '8px' }}
+//       />
+//       <p>Product ID: {productDetails.id}</p>
+//       <p>Title: {productDetails.title}</p>
+//       <button  className="btn btn-success" onClick={()=>addCart(productDetails)}>ADD TO CART</button>
+//       <button  className="btn btn-success" >BUY NOW</button>
+
+
+//     </div>
+//   );
+// }
+
+// export default Page;
+
+
+
+
+
+
 "use client";
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
+import './product.css';
 
 function Page({ params }) {
   
@@ -28,42 +86,48 @@ function Page({ params }) {
 
 
   const addCart=()=>{
-<<<<<<< HEAD
     Cookies.set("poooja","binjawa")
-    // Cookies.set(productDetails,true)
-    // Cookies.set(`product_${productDetails.id}`, JSON.stringify(productDetails));
-=======
-  
-    
-    Cookies.set(`product_${productDetails.id}`, JSON.stringify(productDetails));
->>>>>>> b0a0f2e7019fbab2cc99b32d4a46925f8a544d0d
 
   }
   return (
-    <div style={{ border: "solid 2px red" }}>
-      <h1>Product Details-</h1>
-      <img
-        src={productDetails.image}
-        style={{ width: '350px', height: '300px', borderRadius: '8px' }}
-      />
-      <p>Product ID: {productDetails.id}</p>
-      <p>Title: {productDetails.title}</p>
-<<<<<<< HEAD
-      <button  className="btn btn-success" >ADD TO CART</button>
+    <div className='container'>
+      <div className='imagee'>
+        <img
+          src={productDetails.image}
+          alt={productDetails.title}
+        />
+      </div>
 
-      {/* <button  className="btn btn-success" onClick={()=>addCart(productDetails)}>ADD TO CART</button> */}
-      <button  className="btn btn-success" >BUT NOW</button>
-=======
-      <button  className="btn btn-success" onClick={()=>addCart(productDetails)}>ADD TO CART</button>
-      <button  className="btn btn-success" >BUY NOW</button>
->>>>>>> b0a0f2e7019fbab2cc99b32d4a46925f8a544d0d
+      <div className='data'>
+        <h1>Product Details</h1>
+        <p>Product ID: {productDetails.id}</p>
+        <p>Title: {productDetails.title}</p>
+        <button  className="btn btn-success" >ADD TO CART</button>
 
-
+      </div>
     </div>
+  
+
+
+
+    // <div className='container'>
+    //   <h1>Product Details-</h1>
+    //   <img
+    //     src={productDetails.image}
+    //     style={{ width: '350px', height: '300px', borderRadius: '8px' }}
+    //   />
+    //   <p>Product ID: {productDetails.id}</p>
+    //   <p>Title: {productDetails.title}</p>
+    //   <button  className="btn btn-success" onClick={()=>addCart(productDetails)}>ADD TO CART</button>
+    //   <button  className="btn btn-success" >BUY NOW</button>
+
+
+    // </div>
   );
 }
 
 export default Page;
+
 
 
 
